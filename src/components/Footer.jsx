8d8028ced1d9b237f7bdc574ace7332c122e61bd
Sride Shankar <sridevishankar1161@gmail.com>
2024-06-16 10:53:06 +0530

    final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <div>
         <h4>Copyright © 2024        Sridevi</h4>
-        <a href="https://www.github.com/srii5477" class="fa fa-github"></a>
-        <a href="https://www.linkedin.com/in/sridevi-s-636a33255" class="fa fa-linkedin"></a>
+        <a href="https://www.github.com/srii5477" class="fa fa-github">.</a>
+        <a href="https://www.linkedin.com/in/sridevi-s-636a33255" class="fa fa-linkedin">.</a>
         
     </div>
   );
